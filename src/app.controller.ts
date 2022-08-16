a1f4245509d909387b1cbe6e3980704bd3f5ec1e
kaushikkishore <kaushik@vauld.com>
2022-08-16 17:38:56 +0530

    app controller change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealth(): string {
+    return this.appService.getHealth();
   }
+
+  @Get("/version")
+  getVersion(): string {
+    return this.appService.getHealth();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealth(): string {
+    return 'Ok!';
+  }
+
+  getVersion(): string {
+    return "1.0.0.0"
   }
 }
