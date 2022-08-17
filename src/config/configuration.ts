85cf9a5170beb143f66c4873f56090ffecec4661
kaushikkishore <kaushik@vauld.com>
2022-08-17 21:39:27 +0530

    updated config files

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,4 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     connectionString: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.fguqtla.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
   },
